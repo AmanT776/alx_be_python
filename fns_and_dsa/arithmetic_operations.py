1530753d2a5bbb5d81dd6696af890bd48bb1618b
AmanT776 <amantes776@gmail.com>
2025-07-27 14:33:20 +0300

    fixed some errors

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,12 +1,12 @@
 def perform_operation(num1,num2,operation):
     match operation:
-        case '+':
+        case 'add':
             print(num1+num2)
-        case '-':
+        case 'subtract':
             print(num1-num2)
-        case '*':
+        case 'multiply':
             print(num1*num2)
-        case '/':
+        case 'divide':
             if(num2 == 0):
                 print("error: division by zero")
             else:
