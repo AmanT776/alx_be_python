b0dc0fb2cf6b47cc4b64580e91ea598034c3a292
AmanT776 <amantes776@gmail.com>
2025-07-27 14:37:10 +0300

    submit assignment

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,14 +1,15 @@
 def perform_operation(num1,num2,operation):
     match operation:
-        case 'add':
-            print(num1+num2)
-        case 'subtract':
-            print(num1-num2)
-        case 'multiply':
-            print(num1*num2)
-        case 'divide':
+        case '+':
+            return num1+num2 
+        case '-':
+            return num1-num2
+        case '*':
+            return num1*num2
+        case '/':
             if(num2 == 0):
-                print("error: division by zero")
+                return "error: division by zero"
             else:
-                print(num1/num2)
+                return num1/num2
+
 
