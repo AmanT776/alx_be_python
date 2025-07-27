6c22dd636014763f90890a547a6f4da46ba6f60c
AmanT776 <amantes776@gmail.com>
2025-07-27 15:25:41 +0300

    submit assignment

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -13,13 +13,13 @@ def main():
 
         if choice == '1':
             # Prompt for and add an item
-            item = input("enter an item: ")
+            item = input("Enter the item to add: ")
             shopping_list.append(item)
             print(f"{item} is added to shopping list")
             pass
         elif choice == '2':
             # Prompt for and remove an item
-            item = input("enter an item to remove: ")
+            item = input("Enter an item to remove: ")
             shopping_list.remove(item)
             print(f"{item} removed from shopping list")
             pass
