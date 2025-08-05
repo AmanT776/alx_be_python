da6beae63ac4ecdccceaec2fd658ddeca7c1a184
AmanT776 <amantes776@gmail.com>
2025-08-05 13:37:15 +0300

    fixed: object instantiation

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -4,12 +4,14 @@ from simple_calculator import SimpleCalculator
 calculator = SimpleCalculator()
 
 class TestSimpleCalculator(unittest.TestCase):
-    def test_add(self):
-        self.assertEqual(calculator.add(1,2),3)
-    def test_subtract(self):
-        self.assertEqual(calculator.subtract(2,1),1)
-    def text_multiplication(self):
-        self.assertEqual(calculator.multiply(2,2),4)
-    def test_division(self):
-        self.assertEqual(calculator.divide(4,2),2)
-        self.assertEqual(calculator.divide(2,0),None)
+    def setUp(self):
+        self.calc = SimpleCalculator()
+    def test_addition(self):
+        self.assertEqual(self.calc.add(1,2),3)
+    def test_subtraction(self):
+        self.assertEqual(self.calc.subtract(2,1),1)
+    def text_multiply(self):
+        self.assertEqual(self.calc.multiply(2,2),4)
+    def test_divide(self):
+        self.assertEqual(self.calc.divide(4,2),2)
+        self.assertEqual(self.calc.divide(2,0),None)
