05491f1562ae14d7a73dd54e202738eaf79b2416
AmanT776 <amantes776@gmail.com>
2025-08-05 13:40:13 +0300

    fix

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -10,7 +10,7 @@ class TestSimpleCalculator(unittest.TestCase):
         self.assertEqual(self.calc.add(1,2),3)
     def test_subtraction(self):
         self.assertEqual(self.calc.subtract(2,1),1)
-    def text_multiplication(self):
+    def test_multiplication(self):
         self.assertEqual(self.calc.multiply(2,2),4)
     def test_division(self):
         self.assertEqual(self.calc.divide(4,2),2)
