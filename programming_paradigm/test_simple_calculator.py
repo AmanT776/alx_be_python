1334846ae5459f7efa56d160a6cbdb61a7ed19a4
AmanT776 <amantes776@gmail.com>
2025-08-05 13:38:29 +0300

    fix: fixed naming for multiply and divide methods

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -10,8 +10,8 @@ class TestSimpleCalculator(unittest.TestCase):
         self.assertEqual(self.calc.add(1,2),3)
     def test_subtraction(self):
         self.assertEqual(self.calc.subtract(2,1),1)
-    def text_multiply(self):
+    def text_multiplication(self):
         self.assertEqual(self.calc.multiply(2,2),4)
-    def test_divide(self):
+    def test_division(self):
         self.assertEqual(self.calc.divide(4,2),2)
         self.assertEqual(self.calc.divide(2,0),None)
